7e9fb7c7b33cc0dad51650c1031332dc6ec6dad9
Luis Manon <soypromord@gmail.com>
2025-08-13 17:32:08 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.js
@@ -0,0 +1,36 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.virtuscarrentalsrl.com',
+      },
+       {
+        protocol: 'https',
+        hostname: 'virtus-version-ok.appspot.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'virtus-version-ok.firebasestorage.app',
+      }
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin'],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      "*.cloudworkstations.dev",
+      "*.firebase.dev"
+    ],
+  },
+};
+
+export default nextConfig;
